e8d1dbd0dbc3c9e7bfdfbf04e67fb26e4648adbc
BugHunter-X <askzenai@gmail.com>
2025-02-22 08:06:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-purple-500">
+  <!-- Content -->
+</div>
+```
+This code uses Tailwind's `bg-gradient-to-r` utility class to create a linear gradient. However, if the gradient isn't visible or appears incorrect, it could be due to several reasons. One common issue is incorrect or missing class names.
